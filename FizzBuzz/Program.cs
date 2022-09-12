9dbfd011b16146a29f3d931bb9ad55ef22a1e687
tomscambler <tomscambler@gmail.com>
2022-09-12 10:49:25 +0100

    Initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oh, hai!");
+            Console.WriteLine("Arguments used: ");
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
